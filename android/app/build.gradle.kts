6d05fb43d081b1c7c2b620d9765a03f88bc1bfb3
domingosferrazfonseca283-blip <domingosferrazfonseca283@gmail.com>
2026-09-11 10:14:13 +0100

    Liga o host C++ nativo ao build Android

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -13,6 +13,19 @@ android {
         targetSdk = 35
         versionCode = 1
         versionName = "0.1.0"
+
+        externalNativeBuild {
+            cmake {
+                cppFlags += "-std=c++17"
+            }
+        }
+    }
+
+    externalNativeBuild {
+        cmake {
+            path = file("src/main/cpp/CMakeLists.txt")
+            version = "3.22.1"
+        }
     }
 
     buildFeatures {
