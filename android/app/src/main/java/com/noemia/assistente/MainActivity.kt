4b4769b8ea3d1e75173757a39aeeb64a18116bc7
domingosferrazfonseca283-blip <domingosferrazfonseca283@gmail.com>
2026-09-11 13:28:54 +0100

    Inicia o corpo persistente da Noémia como foreground service

--- android/app/src/main/java/com/noemia/assistente/MainActivity.kt
+++ android/app/src/main/java/com/noemia/assistente/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.noemia.assistente
 
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        startService(Intent(this, NoemiaRuntimeService::class.java))
+        val intent = Intent(this, NoemiaRuntimeService::class.java)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            startForegroundService(intent)
+        } else {
+            startService(intent)
+        }
     }
 }
