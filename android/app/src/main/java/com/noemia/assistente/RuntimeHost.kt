ef5e9158ee1b726de8f313b69675d5c3d5ffac47
domingosferrazfonseca283-blip <domingosferrazfonseca283@gmail.com>
2026-09-11 09:52:54 +0100

    Liga o host Android ao transporte JNI local

--- android/app/src/main/java/com/noemia/assistente/RuntimeHost.kt
+++ android/app/src/main/java/com/noemia/assistente/RuntimeHost.kt
@@ -2,7 +2,7 @@ package com.noemia.assistente
 
 /** Seleciona explicitamente o hospedeiro do núcleo cognitivo. */
 object RuntimeHost {
-    fun createTransport(): LocalRuntimeTransport = UnavailableLocalRuntimeTransport()
+    fun createTransport(): LocalRuntimeTransport = NativeLocalRuntimeTransport()
 
     fun createBridge(): CognitiveBridge = ProtocolCognitiveBridge(createTransport())
 }
