19908129045cb7f400bfb8b8d22b4a15e20127e7
domingosferrazfonseca283-blip <domingosferrazfonseca283@gmail.com>
2026-09-12 22:36:16 +0100

    body: export runtime bridge

--- body/__init__.py
+++ body/__init__.py
@@ -1,5 +1,6 @@
 """Corpo universal e abstrações de hardware."""
 
 from .interfaces import Actuator, Body, BodyAdapter, Sense
+from .runtime import BodyRuntime
 
-__all__ = ["Actuator", "Body", "BodyAdapter", "Sense"]
+__all__ = ["Actuator", "Body", "BodyAdapter", "BodyRuntime", "Sense"]
